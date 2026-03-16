e08902d35c5db6080347e3b491c5c54d067b53b2
ihsaan31 <ihsanzombieihsan@gmail.com>
2026-03-16 16:10:12 +0700

    gemini 3 flash

--- app.py
+++ app.py
@@ -567,9 +567,11 @@ with st.sidebar:
 
     model_choice = st.selectbox(
         "Model",
-        [
+        [   
+            "gemini-3.1-flash-image-preview",
             "gemini-3-pro-image-preview",
-            "gemini-2.5-flash-image"
+            "gemini-2.5-flash-image",
+            
         ],
         index=0
     )
